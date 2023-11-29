3342135399eb0e6fa21c6d3bd022381ae018363f
escobaredison <escobaredison2001@gmail.com>
2023-11-28 21:28:52 -0500

    finalDos

--- src/components/Visit/Visit.jsx
+++ src/components/Visit/Visit.jsx
@@ -62,25 +62,25 @@ const Visit = () => {
                             ))
                         }
                     </div>
+                    <Link className='return-menu' to="/">
+                        <button className='select-buton'>MENU</button>
+                    </Link>
+                    <div className='parrafo'> 
+                        <div className='espacio-parrafo-uno'>Geografía:</div>
+                        <div className='espacio-parrafo'>Rondón tradicionalmente sigue ostentando sus linderos declarados en la ordenanza 05 de 1904 y los incorporados mediante el Decreto Eclesiástico 122 de 1920, por el cual fue creada la parroquia de San Rafael, hoy Rondón. El municipio de Rondón está dividido en 12 veredas que son: Centro, Bolívar, Sucre, Nueva Granada, Junín, Junín Vásquez, San José, San Antonio, San Isidro, Nariño, Ricaurte y San Ignacio.</div>
+                        <div className='espacio-parrafo-uno'>Límites del municipio:</div>
+                        <div className='espacio-parrafo'>Por el Norte : Viracachá y Siachoque</div>
+                        <div className='espacio-parrafo'>Por el sur : Zetaquira y Ramiriquí</div>
+                        <div className='espacio-parrafo'>Por el oriente : Pesca</div>
+                        <div className='espacio-parrafo'>Por el occidente : Ramiriquí y Ciénega</div>
+                        <div className='espacio-parrafo'>Extensión total: 258 Km2</div>
+                        <div className='espacio-parrafo'>Extensión área urbana: 0.8 Km2</div>
+                        <div className='espacio-parrafo'>Extensión área rural: 257.2 Km2</div>
+                        <div className='espacio-parrafo'>Altitud de la cabecera municipal (metros sobre el nivel del mar):  2075</div>
+                        <div className='espacio-parrafo'>Temperatura media:  17º C</div>
+                        <div className='espacio-parrafo'>Distancia de referencia:  61 km de la capital del Departamento</div>
+                    </div>
                 </div>
-            </div>
-            <Link className='return-menu' to="/">
-                <button className='select-buton'>MENU</button>
-            </Link>
-            <div className='parrafo'> 
-                <div className='espacio-parrafo-uno'>Geografía:</div>
-                <div className='espacio-parrafo'>Rondón tradicionalmente sigue ostentando sus linderos declarados en la ordenanza 05 de 1904 y los incorporados mediante el Decreto Eclesiástico 122 de 1920, por el cual fue creada la parroquia de San Rafael, hoy Rondón. El municipio de Rondón está dividido en 12 veredas que son: Centro, Bolívar, Sucre, Nueva Granada, Junín, Junín Vásquez, San José, San Antonio, San Isidro, Nariño, Ricaurte y San Ignacio.</div>
-                <div className='espacio-parrafo-uno'>Límites del municipio:</div>
-                <div className='espacio-parrafo'>Por el Norte : Viracachá y Siachoque</div>
-                <div className='espacio-parrafo'>Por el sur : Zetaquira y Ramiriquí</div>
-                <div className='espacio-parrafo'>Por el oriente : Pesca</div>
-                <div className='espacio-parrafo'>Por el occidente : Ramiriquí y Ciénega</div>
-                <div className='espacio-parrafo'>Extensión total: 258 Km2</div>
-                <div className='espacio-parrafo'>Extensión área urbana: 0.8 Km2</div>
-                <div className='espacio-parrafo'>Extensión área rural: 257.2 Km2</div>
-                <div className='espacio-parrafo'>Altitud de la cabecera municipal (metros sobre el nivel del mar):  2075</div>
-                <div className='espacio-parrafo'>Temperatura media:  17º C</div>
-                <div className='espacio-parrafo'>Distancia de referencia:  61 km de la capital del Departamento</div>
             </div>
         </div>
     )
